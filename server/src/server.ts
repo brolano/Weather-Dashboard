e8fe0b1ecf08895c070598daf7e8c264403d4545
Brian Solano <brian_solano110012@yahoo.com>
2024-11-27 00:03:05 -0600

    completed server ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 dotenv.config();
 
 // Import the routes
@@ -10,11 +11,15 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // TODO: Implement middleware to connect the routes
-app.use(routes);
+app.use(routes); (express.json)
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
