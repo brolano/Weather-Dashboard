e65711876b977054a0b657785ce06f6f557d84d3
Brian Solano <brian_solano110012@yahoo.com>
2024-11-28 22:24:10 -0600

    reverted changes as it caused more render issues

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,18 @@
 import dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 dotenv.config();
-
-// Import the routes
 import routes from './routes/index.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
 
 // TODO: Serve static files of entire client dist folder
-const distPath = path.join(__dirname, 'dist');
-app.use(express.static(distPath));
+
+app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), 'dist')));
 
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
@@ -26,11 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // TODO: Implement middleware to connect the routes
-app.use(routes);
-
-app.get('*', (_req: Request, res: Response) => {
-    res.sendFile(path.join(distPath, 'index.html'));
-});
+app.use(routes); (express.json)
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
